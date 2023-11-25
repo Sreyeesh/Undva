1dec7f67e464cf2966a91ccfc7aec8f75272a34d
Sreyeesh <sgarime1@gmail.com>
2023-11-25 23:20:33 +0200

    re-design is done for the main page

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 // script.js
-window.onscroll = function() {
-    var nav = document.querySelector('nav');
-    if (window.pageYOffset > 100) {
-        nav.classList.add('fixed');
+let currentSlideIndex = 0;
+
+function changeSlide(direction) {
+  const slides = document.querySelector('.slides');
+  const totalSlides = document.querySelectorAll('.slide').length;
+  
+  currentSlideIndex += direction;
+
+  // If we go beyond the first or last slide, loop around to the other end
+  if (currentSlideIndex >= totalSlides) currentSlideIndex = 0;
+  if (currentSlideIndex < 0) currentSlideIndex = totalSlides - 1;
+
+  // Translate the slides container
+  slides.style.transform = `translateX(-${currentSlideIndex * 100}%)`;
+}
+
+// Attach event listeners to arrows
+document.querySelector('.left-arrow').addEventListener('click', () => changeSlide(-1));
+document.querySelector('.right-arrow').addEventListener('click', () => changeSlide(1));
+
+
+// Function to handle the scroll event
+function handleScroll() {
+    const navbar = document.getElementById("navbar");
+    const scrollY = window.scrollY;
+
+    // Add the "fixed" class when scrolling down, remove it when at the top
+    if (scrollY > 0) {
+        navbar.classList.add("fixed");
     } else {
-        nav.classList.remove('fixed');
+        navbar.classList.remove("fixed");
     }
-};
+}
+
+// Attach the scroll event listener
+window.addEventListener("scroll", handleScroll);
